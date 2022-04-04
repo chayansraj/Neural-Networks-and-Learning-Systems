54cc110d769740653bd9e2c9e54cb8dbe6b4dfb7
Theodor Emanuelsson <49917684+TheodorEmanuelsson@users.noreply.github.com>
2022-04-04 20:58:04 +0200

    Add files via upload

---
+++ ResubLab4/utils.py
@@ -0,0 +1,48 @@
+
+import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits import mplot3d
+
+
+def getpolicy(Q):
+    """ GWGETPOLICY
+    Get best policy matrix from the Q-matrix.
+    You have to implement this function yourself. It is not necessary to loop
+    in order to do this, and looping will be much slower than using matrix
+    operations. It's possible to implement this in one line of code.
+    """
+
+    #P = np.amax(Q, axis = 2)
+    P = np.argmax(Q, axis = 2) 
+
+    return P
+
+
+def getvalue(Q):
+    """ GWGETVALUE
+    Get best value matrix from the Q-matrix.
+    You have to implement this function yourself. It is not necessary to loop
+    in order to do this, and looping will be much slower than using matrix
+    operations. It's possible to implement this in one line of code.
+    """
+    
+    V = np.max(Q)
+
+    return V
+
+def plotarrows(P):
+    """ PLOTARROWS
+    Displays a policy matrix as an arrow in each state.
+    """
+
+    x,y = np.meshgrid(np.arange(P.shape[1]), np.arange(P.shape[0]))
+
+    u = np.zeros(x.shape)
+    v = np.zeros(y.shape)
+
+    v[P==2] = 1
+    v[P==3] = -1
+    u[P==0] = -1
+    u[P==1] = 1
+
+    plt.quiver(v,u,color='r')
